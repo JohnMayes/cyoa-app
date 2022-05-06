0d0190462ddedfca4ed5cdeec8a4a7988951dca1
JohnMayes <JohnMayes51@gmail.com>
2022-05-06 16:41:07 -0500

    sidebar improvemnets

--- adventure/Container.svelte
+++ adventure/Container.svelte
@@ -5,38 +5,38 @@
   export let Link, state, current_page_name;
 </script>
 
-<header>
-  <h1>Treasures of the Cursed Pyramid</h1>
-</header>
-
 <div class="container">
-  <div class="sidebar">
-    <p>Dexterity: {$state.stats.dex}</p>
-    <p>Strength: {$state.stats.str}</p>
-    <span>Charm: {$state.stats.cha}</span>
-    <span>Rations: {$state.stats.rat}</span>
+  <div class="sidebar-container">
+    <div class="sidebar-content">
+      <p>Dexterity: {$state.stats.dex}</p>
+      <p>Strength: {$state.stats.str}</p>
+      <span>Charm: {$state.stats.cha}</span>
+      <span>Rations: {$state.stats.rat}</span>
+    </div>
   </div>
+
   <div class="section">
+    <header>
+      <h1>Treasures of the Cursed Pyramid</h1>
+    </header>
     <slot />
+    <footer>
+      {#if $current_page_name === `Inventory`}
+        <span class="currently_on">Inventory</span>
+      {:else}
+        <Link to="Inventory">Inventory</Link>
+      {/if}
+
+      <Save {Link} {state} {current_page_name} />
+
+      <Link to="Start" state={initial_state}>Reset</Link>
+    </footer>
   </div>
-
-  <footer>
-    {#if $current_page_name === `Inventory`}
-      <span class="currently_on">Inventory</span>
-    {:else}
-      <Link to="Inventory">Inventory</Link>
-    {/if}
-
-    <Save {Link} {state} {current_page_name} />
-
-    <Link to="Start" state={initial_state}>Reset</Link>
-  </footer>
 </div>
 
 <style>
   .container {
-    display: flex;
-    flex-direction: row;
+    height: 100vh;
   }
 
   .section {
@@ -58,16 +58,27 @@
     --gray: #939393;
   }
 
-  .sidebar {
+  .sidebar-container {
     display: flex;
     flex-direction: column;
-    position: fixed;
-    left: 0;
-    top: 0;
     height: 100vh;
-    width: 15vw;
-    background-color: #939393;
-    margin-right: 10vw;
+    width: 16rem;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    border-right: 2px black;
+  }
+
+  .sidebar-content {
+    padding-top: 1.25rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    align-items: center;
+    background-color: whitesmoke;
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    overflow-y: auto;
   }
 
   footer {
